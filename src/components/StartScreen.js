ac0e41b3176b45b24cc20ea3dbf3e94cf5f6d2c9
Benny Chrispin <ndizibaidu23@gmail.com>
2023-11-13 01:13:40 +0200

    Initial commit

---
+++ src/components/StartScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function StartScreen({ numQuestions, dispatch }) {
+  return (
+    <div className="start">
+      <h2>Welcome to The React Quiz!</h2>
+      <h3>{numQuestions} Question to test your React mastery</h3>
+      <h4>Good luck!</h4>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "start" })}
+      >
+        Get Started
+      </button>
+    </div>
+  );
+}
